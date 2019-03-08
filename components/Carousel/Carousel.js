ff16ced4dfb713f8b59203778f42d85b5ca84e34
Henry Blevins <heblevi@gmail.com>
2019-03-08 11:14:53 -0500

    Carousel MVP

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,24 @@
 class Carousel {
+  constructor(carousel) {
+    this.element = carousel;
+    this.left = this.element.querySelector('.left-button');
+    this.right = this.element.querySelector('.right-button');
+    this.images = this.element.querySelectorAll('img');
+    // Select first image by default
+    this.selected = 0;
+    this.images[this.selected].classList.add('carousel-show');
 
+    this.left.addEventListener('click', () => this.iterImage(-1));
+    this.right.addEventListener('click', () => this.iterImage(1));
+  }
+  iterImage(num) {
+    this.images[this.selected].classList.remove('carousel-show');
+    this.selected = (this.images.length + (this.selected + (num % this.images.length))) % this.images.length;
+    this.images[this.selected].classList.add('carousel-show');
+  }
 }
 
-let carousel = document.querySelector();
+let carousel = new Carousel(document.querySelector('.carousel'));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
